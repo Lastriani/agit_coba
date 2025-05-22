ffe0020315edf625fca5598e529a7fc26b464674
Lastri Ani <lastriani510@gmail.com>
2025-05-22 09:17:14 +0700

    tambah tahun

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Saya sudah jomblo hapir 3 tahun");
+Console.WriteLine("Saya sudah jomblo hapir 8000 tahun");
